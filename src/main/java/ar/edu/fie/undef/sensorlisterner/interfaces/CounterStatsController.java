16872d4c799c231d14ba199a38d1e713a435d0c2
Jonathan Curellich <viking@MBP-de-Jonathan.glc>
2024-02-20 11:19:55 -0300

    Add CounterStats controller to get all CounterStats

--- src/main/java/ar/edu/fie/undef/sensorlisterner/interfaces/CounterStatsController.java
+++ src/main/java/ar/edu/fie/undef/sensorlisterner/interfaces/CounterStatsController.java
@@ -1,4 +1,24 @@
 package ar.edu.fie.undef.sensorlisterner.interfaces;
 
+import ar.edu.fie.undef.sensorlisterner.infrastruture.CounterStatsRepository;
+import ar.edu.fie.undef.sensorlisterner.interfaces.responses.StandardResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class CounterStatsController {
+
+    private final CounterStatsRepository counterStatsRepository;
+
+    public CounterStatsController(CounterStatsRepository counterStatsRepository) {
+        this.counterStatsRepository = counterStatsRepository;
+    }
+    @GetMapping("api/v1/counterstats")
+    public ResponseEntity<StandardResponse> getCounterStats() {
+        var counterStats = counterStatsRepository.findAllByOrderByIdDesc();
+        return new ResponseEntity<>(new StandardResponse(true, counterStats.size() + " Counter found", counterStats), HttpStatus.OK);
+    }
+
 }
